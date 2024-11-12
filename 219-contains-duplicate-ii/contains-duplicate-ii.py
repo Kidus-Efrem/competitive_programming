24f3d2f4b974cfb73c429296eea12b9ebf533825
Kidus-Efrem <105239743+Kidus-Efrem@users.noreply.github.com>
2024-11-12 13:53:59 +0300

    Time: 24 ms (67.07%) | Memory: 20.9 MB (62.84%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         num_indcies = {}
         for i in range(len(nums)):
-            if nums[i] in num_indcies and abs(i - num_indcies[nums[i]])<=k:
+            if nums[i] in num_indcies and i - num_indcies[nums[i]]<=k:
                 return True
             num_indcies[nums[i]]=i
         return False
