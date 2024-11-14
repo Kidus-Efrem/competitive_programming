0370d5d2d25336be99c393283a2bcbc6be276cbd
Kidus-Efrem <105239743+Kidus-Efrem@users.noreply.github.com>
2024-11-15 01:40:13 +0300

    Time: 47 ms (5.27%) | Memory: 12 MB (7.59%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,25 @@
+
+class Solution:
+    def generateParenthesis(self, n):
+        res = []
+
+        def valid(s):
+            open = 0
+            for c in s:
+                open += 1 if c == '(' else -1
+                if open < 0:
+                    return False
+            return not open
+
+        def dfs(s):
+            if n * 2 == len(s):
+                if valid(s):
+                    res.append(s)
+                return
+            
+            dfs(s + '(')
+            dfs(s + ')')
+        
+        dfs("")
+        return res
+        
