ff2b00a2accbc55e5318fe2905e65adc0bbc7f6e
Kidus-Efrem <105239743+Kidus-Efrem@users.noreply.github.com>
2024-11-15 01:41:48 +0300

    Time: 4 ms (20.73%) | Memory: 11.8 MB (84.40%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,25 +1,21 @@
-
 class Solution:
     def generateParenthesis(self, n):
+        stack = []
         res = []
 
-        def valid(s):
-            open = 0
-            for c in s:
-                open += 1 if c == '(' else -1
-                if open < 0:
-                    return False
-            return not open
-
-        def dfs(s):
-            if n * 2 == len(s):
-                if valid(s):
-                    res.append(s)
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
                 return
-            
-            dfs(s + '(')
-            dfs(s + ')')
-        
-        dfs("")
+
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+        backtrack(0, 0)
         return res
-        
