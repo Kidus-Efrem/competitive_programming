faa0e89ccf69f572ec620e557e2097435df80c2c
Kidus-Efrem <105239743+Kidus-Efrem@users.noreply.github.com>
2024-11-15 02:01:34 +0300

    Time: 390 ms (10.37%) | Memory: 26.5 MB (39.13%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,65 +1,28 @@
-class MinIdx_Segtree:
-    def __init__(self, N, A):
-        self.n = N
-        self.INF = int(1e9)
-        self.A = A
-        while (self.n & (self.n - 1)) != 0:
-            self.A.append(self.INF)
-            self.n += 1
-        self.tree = [0] * (2 * self.n)
-        self.build()
-
-    def build(self):
-        for i in range(self.n):
-            self.tree[self.n + i] = i
-        for j in range(self.n - 1, 0, -1):
-            a = self.tree[j << 1]
-            b = self.tree[(j << 1) + 1]
-            if self.A[a] <= self.A[b]:
-                self.tree[j] = a
-            else:
-                self.tree[j] = b
-
-    def update(self, i, val):
-        self.A[i] = val
-        j = (self.n + i) >> 1
-        while j >= 1:
-            a = self.tree[j << 1]
-            b = self.tree[(j << 1) + 1]
-            if self.A[a] <= self.A[b]:
-                self.tree[j] = a
-            else:
-                self.tree[j] = b
-            j >>= 1
-
-    def query(self, ql, qh):
-        return self._query(1, 0, self.n - 1, ql, qh)
-
-    def _query(self, node, l, h, ql, qh):
-        if ql > h or qh < l:
-            return self.INF
-        if l >= ql and h <= qh:
-            return self.tree[node]
-        a = self._query(node << 1, l, (l + h) >> 1, ql, qh)
-        b = self._query((node << 1) + 1, ((l + h) >> 1) + 1, h, ql, qh)
-        if a == self.INF:
-            return b
-        if b == self.INF:
-            return a
-        return a if self.A[a] <= self.A[b] else b
-
 class Solution:
-    def getMaxArea(self, heights, l, r, st):
-        if l > r:
-            return 0
-        if l == r:
-            return heights[l]
-        minIdx = st.query(l, r)
-        return max(max(self.getMaxArea(heights, l, minIdx - 1, st),
-                       self.getMaxArea(heights, minIdx + 1, r, st)),
-                   (r - l + 1) * heights[minIdx])
-
     def largestRectangleArea(self, heights):
         n = len(heights)
-        st = MinIdx_Segtree(n, heights)
-        return self.getMaxArea(heights, 0, n - 1, st)
+        stack = []
+
+        leftMost = [-1] * n
+        for i in range(n):
+            while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            if stack:
+                leftMost[i] = stack[-1]
+            stack.append(i)
+        
+        stack = []
+        rightMost = [n] * n
+        for i in range(n - 1, -1, -1):
+            while stack and heights[stack[-1]] >= heights[i]:
+                stack.pop()
+            if stack:
+                rightMost[i] = stack[-1]
+            stack.append(i)
+        
+        maxArea = 0
+        for i in range(n):
+            leftMost[i] += 1
+            rightMost[i] -= 1
+            maxArea = max(maxArea, heights[i] * (rightMost[i] - leftMost[i] + 1))
+        return maxArea
