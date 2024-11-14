a868e9272a5fbcffa13a6e0a85a87caec07ad079
Kidus-Efrem <105239743+Kidus-Efrem@users.noreply.github.com>
2024-11-14 21:37:14 +0300

    Time: 3 ms (46.88%) | Memory: 11.5 MB (93.31%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -4,8 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        while '()' in s or '{}' in s or '[]' in s:
-            s = s.replace('()', '')
-            s = s.replace('{}', '')
-            s = s.replace('[]', '')
-        return s == ''
+        stack = []
+        closeToOpen = { ")" : "(", "]" : "[", "}" : "{" }
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        return True if not stack else False
