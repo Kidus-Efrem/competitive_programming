e32ce10d44aeebfa54a1f30a1698e28e9aab2018
Kidus-Efrem <105239743+Kidus-Efrem@users.noreply.github.com>
2024-11-23 13:34:30 +0300

    Time: 0 ms (100.00%) | Memory: 11.6 MB (59.11%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,18 +4,10 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        n = len(digits)-1
-        print(n, digits[n])
-
-        digits[n] += 1
-        print(n, digits[n])
-        while n >= 0:
-            if digits[n] == 10 and n ==0:
-                digits.insert(0,1)
-                digits[n+1]=0
-                print (n, digits[n])
-            elif digits[n] == 10:
-                digits[n-1] += 1
-                digits[n] = 0
-            n-= 1
-        return digits
+        carry = 1
+        for i in range(len(digits)-1, -1 ,-1):
+            digits[i] += carry
+            if digits[i] < 10:
+                return digits
+            digits[i] =0
+        return [1]+digits
