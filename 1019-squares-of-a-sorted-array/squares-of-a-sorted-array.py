f9f8fbeaecd4b56a2cefda0e9c1a727066ffc0f6
Kidus-Efrem <105239743+Kidus-Efrem@users.noreply.github.com>
2024-11-25 18:18:51 +0300

    Time: 12 ms (49.06%) | Memory: 13.6 MB (58.75%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,10 +1,48 @@
+# class Solution(object):
+#     def sortedSquares(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[int]
+#         """
+#         left = 0
+#         right = len(nums)-1
+#         pos = len(nums)-1
+
+#         while left<=right:
+#             left_squared = nums[left]**2
+#             right_squared = nums[right]**2
+
+#             if (left_squared > right_squared):
+#                 nums[pos] = left_squared
+#                 left+=1
+#             else:
+#                 nums[pos] = right_squared
+#                 right -= 1
+#             pos -= 1
+#         return nums
+        
 class Solution(object):
     def sortedSquares(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            nums[i] = nums[i]**2
-            print (nums[i])
-        return sorted(nums)
+        left = 0
+        right = len(nums) - 1
+        res= [0]*len(nums)
+        pos = len(nums) - 1
+
+        while left <= right:
+            left_squared = nums[left] ** 2
+            right_squared = nums[right] ** 2
+
+
+            if left_squared > right_squared:
+                res[pos] = left_squared
+                left += 1
+            else:
+                res[pos] = right_squared
+                right -= 1
+            pos -= 1  # Move position for the next largest square
+
+        return res
