b69fd44ef1a4c24e2e96705bb6b5a8739cc1d9c0
Kidus-Efrem <105239743+Kidus-Efrem@users.noreply.github.com>
2025-04-09 18:03:07 +0300

    Time: 30 ms (11.44%) | Memory: 22.5 MB (7.80%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -3,13 +3,13 @@ class Solution:
         d = {i:[] for i in range(n)}
         for u, v in trust:
             d[u-1].append(v-1)
-        print('d', d)
+        # print('d', d)
         cnt = {i:[0,0] for i in range(n)}
         for i in range(n):
             cnt[i][0] = len(d[i])
 
         def dfs(vertex, visited):
-            print("cur",vertex)
+            # print("cur",vertex)
             
             visited.add(vertex)
             # cnt[vertex][1]+=1
@@ -24,7 +24,7 @@ class Solution:
         for i in range(n):
             if i  not in visited:
                 dfs(i, visited)
-        print(cnt)
+        # print(cnt)
         for i in range(n):
             l, r  = cnt[i]
             if l==0 and r == n-1:
