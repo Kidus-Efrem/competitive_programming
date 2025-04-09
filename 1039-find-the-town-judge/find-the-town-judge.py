50263f561537ba985af2bd0863c2b409ae73a7fc
Kidus-Efrem <105239743+Kidus-Efrem@users.noreply.github.com>
2025-04-09 18:02:24 +0300

    Time: 59 ms (5.09%) | Memory: 22.8 MB (7.80%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        d = {i:[] for i in range(n)}
+        for u, v in trust:
+            d[u-1].append(v-1)
+        print('d', d)
+        cnt = {i:[0,0] for i in range(n)}
+        for i in range(n):
+            cnt[i][0] = len(d[i])
+
+        def dfs(vertex, visited):
+            print("cur",vertex)
+            
+            visited.add(vertex)
+            # cnt[vertex][1]+=1
+            
+            for neighbour in d[vertex]:
+                if neighbour not in visited:
+                    cnt[neighbour][1]+=1
+                    dfs(neighbour, visited )
+                else:
+                    cnt[neighbour][1]+=1
+        visited = set()
+        for i in range(n):
+            if i  not in visited:
+                dfs(i, visited)
+        print(cnt)
+        for i in range(n):
+            l, r  = cnt[i]
+            if l==0 and r == n-1:
+                return i+1
+        return -1
+
+
+        # print(cnt)
