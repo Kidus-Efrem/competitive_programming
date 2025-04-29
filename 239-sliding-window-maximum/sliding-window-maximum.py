ad9d067442912cced885378ae5b7248cf437d3e5
Kidus-Efrem <105239743+Kidus-Efrem@users.noreply.github.com>
2025-04-29 18:35:48 +0300

    Time: 348 ms (17.28%) | Memory: 35.4 MB (12.61%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,21 +1,36 @@
-class Solution(object):
-    def maxSlidingWindow(self, nums, k):
-        output = []
-        q = deque()  # index
-        l = r = 0
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        arr = []
+        ans = []
+        for i in range(k):
+            heapq.heappush(arr, -nums[i])
+        # print(arr, "first")
+        toremove = defaultdict(int)
+        cnt = 0
+        for j in range(k, len(nums)):
+            while True:
+                temp =  -1* arr[0]
+                if toremove[temp]==0 :
+                    # print(temp)
+                    break
+                temp = heapq.heappop(arr)
+                toremove[-temp]-=1
+            ans.append(temp)
 
-        while r < len(nums):
-            while q and nums[q[-1]] < nums[r]:
-                q.pop()
-            q.append(r)
+            heapq.heappush(arr, -1 * nums[j])
+            toremove[nums[cnt]]+=1
+            cnt+=1
+            # print(arr)
+            # print("to remove", toremove)
 
-            if l > q[0]:
-                q.popleft()
+        while True:
+                temp =  -1* arr[0]
+                if toremove[temp] ==0:
+                    print(temp)
+                    break
+                temp = heapq.heappop(arr)
+                toremove[-temp]-=1
+        ans.append(temp)
 
-            if (r + 1) >= k:
-                output.append(nums[q[0]])
-                l += 1
-            r += 1
 
-        return output
-        
+        return ans
