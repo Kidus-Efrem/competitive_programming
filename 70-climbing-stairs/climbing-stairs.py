200f06c70a897fa70a98142f0335b371e4a52573
Kidus-Efrem <105239743+Kidus-Efrem@users.noreply.github.com>
2025-07-30 23:19:12 +0300

    Time: 0 ms (100.00%) | Memory: 17.5 MB (99.61%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,32 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo  = defaultdict(int)
-        def helper(total):
-            if total in memo:
-                return memo[total]
-            if total ==n:
-                return 1
-            if total >n:
-                return 0
-
-            result = 0
-            result +=backtrack(total+1)
-            result +=backtrack(total+2)
-            if total not in memo:
-                memo[total] = result
-            return memo[total]
-        def helper2():
+        def iter():
             dp = [0 for i in range(n+1)]
-            dp[0] = 1
-            for i in range(n+1):
-                if i+1 <len(dp):
-                    dp[i+1]+=dp[i]
-                if i+2 <len(dp):
-                    dp[i+2]+=dp[i]
-            return dp[-1]
-
-        ans = helper2()
-
+            dp[n] = 1
+            for i in range(n, -1, -1):
+                for  j in range(1, 3):
+                    if i+j < n+1:
+                        dp[i] +=dp[i+j]
+            return dp[0]
+        ans = iter()
         return(ans)
-        
-            
